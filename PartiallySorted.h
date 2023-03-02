04361fe43d0e22dcdbc2a3ea3ff34424bba1e60f
masoncampbell25 <126825096+masoncampbell25@users.noreply.github.com>
2023-03-02 15:36:16 -0600

    Update PartiallySorted.h

--- PartiallySorted.h
+++ PartiallySorted.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 /*
 * TODO comment
 */
 class PartiallySortedList {
-
+private:
+	// fields
+	string* data; 
+	int size; // TODO initialize
+	int capacity; // TODO initialize
+	
+public:
 };
+
