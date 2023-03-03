cf11cb20e38d22a45800c6ebf8869e7acebc8ca9
masoncampbell25 <126825096+masoncampbell25@users.noreply.github.com>
2023-03-03 10:43:25 -0600

    Update PartiallySorted.h
    Added a case where size is 0 and there is nothing in the arraylist so value needs to be entered into index one of the arraylist.

--- PartiallySorted.h
+++ PartiallySorted.h
@@ -66,8 +66,8 @@ public:
 	}
 
 	/*
-	* Either the value is going to be added before another string because 
-	* it has the same length or a larger length than value's length. Otherwise it should 
+	* Either the value is going to be added before another string because
+	* it has the same length or a larger length than value's length. Otherwise it should
 	* be added at the end of the arraylist.
 	*/
 	bool add(string value) {
@@ -76,32 +76,42 @@ public:
 			this->expand();
 		}
 
-		// Loop through stringData until a suitable spot is found
-		for (int i = 0; i < size; i++) {
-			// Check if the value should be inserted at the given index or continue looping
-			if (stringData[i].length >= value.length) {
-				// Loop to make room for the new value being inserted
-				for (int j = size - 1; j > i; j--) { // FIXME - check if condition makes sense
-					// Moves the current index back one slot in the array
-					stringData[j] = stringData[j - 1];
-				}
-				// Enters in the new value at the avalible index
-				stringData[i] = value;
-				// Increment size since a new value was added
-				size++;
-				// use return to end the loop early since we only need to insert one value at a time
-				return false; // could be used to make the distinction of when only part of the loop was used
-			} 
+		// Check if size is equal to 0 and there is nothing to compare values length to
+		if (size == 0) {
+			// insert value at the first slot in the arraylist since it is currently empty
+			stringData[0] = value;
+			// increment size since a new value was added
+			size++;
+			return false; // return false because the loop isn't used
 		}
+		else {
+			// Loop through stringData until a suitable spot is found
+			for (int i = 0; i < size; i++) {
+				// Check if the value should be inserted at the given index or continue looping
+				if (stringData[i].length >= value.length) {
+					// Loop to make room for the new value being inserted
+					for (int j = size - 1; j > i; j--) { // FIXME - check if condition makes sense
+						// Moves the current index back one slot in the array
+						stringData[j] = stringData[j - 1];
+					}
+					// Enters in the new value at the avalible index
+					stringData[i] = value;
+					// Increment size since a new value was added
+					size++;
+					// use return to end the loop early since we only need to insert one value at a time
+					return false; // could be used to make the distinction of when only part of the loop was used
+				}
+			}
 
-		/*
-		* If the loop doesn't find a spot for value, then value must have a length 
-		* larger than the rest and should be put at the end.
-		*/
-		stringData[size] = value;
-		// Increment size since a new value was added
-		size++;
-		return true; // could be used to make a distinction of when the entire loop was used
+			/*
+			* If the loop doesn't find a spot for value, then value must have a length
+			* larger than the rest and should be put at the end.
+			*/
+			stringData[size] = value;
+			// Increment size since a new value was added
+			size++;
+			return true; // could be used to make a distinction of when the entire loop was used
+		}
 	}
 	
 	string get(int index) {
