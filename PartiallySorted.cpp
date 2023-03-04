0e48599f047323bd2f27e8d5ecdd017b01f6eef6
Campbell <campbellm7900@my.uwstout.edu>
2023-03-04 12:35:07 -0600

    testing start

--- PartiallySorted.cpp
+++ PartiallySorted.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include<string>
+#include "PartiallySorted.h"
 using namespace std;
 
 int main() {
-	// test add
-	
-	// test get
+	// Test partially sorted list
+	// Test get - returns expected value
+	// Test getSize - returns expected size(could test in add)
+	// Test add - expand? - size 0 return false - check if it made it to a certain char ending
+	// Test Count permutations - check if grouplist is size 0
+
+	// Test partially sorted String
+	// test getters
+	// test setters
+	// test == - true if same length and same characters in order
+	// test |= - true if same length
+	// test > - if first is greater than 
+	// test < - if first is less than
 }
