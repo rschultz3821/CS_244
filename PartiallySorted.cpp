042395fe2c2b061ff20643af2fbc8d93766ff7f9
Schultz <schultzr3821@my.uwstout.edu>
2023-03-05 12:03:17 -0600

    updated testing

--- PartiallySorted.cpp
+++ PartiallySorted.cpp
@@ -3,6 +3,53 @@
 #include "PartiallySorted.h"
 using namespace std;
 
+void PartiallySortedStringTesting() {
+	// Test partially sorted String
+	cout << "PartiallySortedString tests" << endl << "************" << endl;
+
+	// test getters
+	PartiallySortedString str1{ "testing" };
+	if (str1.getStr() != "testing") {
+		throw "Get string is not working right.";
+	}
+	else {
+		cout << "PartiallySortedString getter working" << endl;
+	}
+
+	// test setters
+	str1.setStr("setter");
+	if (str1.getStr() != "setter") {
+		throw "Set string is not working right.";
+	}
+	else {
+		cout << "PartiallySortedString setter working" << endl;
+	}
+	// test == - true if same length and same characters in order
+	PartiallySortedString str2{ "tree" };
+	PartiallySortedString str3{ "tree" };
+	if (str2 == str3) {
+		cout << "Operator== success" << endl;
+	}
+
+	// test |= - true if same length
+	str3.setStr("test");
+	if (str2 |= str3) {
+		cout << "Operator|= success" << endl;
+	}
+
+	// test > - if first is greater than 
+	str3.setStr("tye");
+	if (str2 > str3) {
+		cout << "Operator> success" << endl;
+	}
+
+	// test < - if first is less than
+	str3.setStr("testing");
+	if (str2 < str3) {
+		cout << "Operator< success" << endl;
+	}
+}
+
 int main() {
 	// Test partially sorted list
 	// Test get - returns expected value
@@ -10,17 +57,5 @@ int main() {
 	// Test add - expand? - size 0 return false - check if it made it to a certain char ending
 	// Test Count permutations - check if grouplist is size 0
 
-	// Test partially sorted String
-	cout << "PartiallySortedString tests" << endl;
-	// test getters
-	// test setters
-	// test == - true if same length and same characters in order
-	PartiallySortedString str = PartiallySortedString {"testing"};
-	if(str.getStr() != "testing"){
-		
-	}
-
-	// test |= - true if same length
-	// test > - if first is greater than 
-	// test < - if first is less than
-}
+	PartiallySortedStringTesting();
+};
